56bc2751a577341a4c24cd8322176a2b2129209b
fosteradorkor <fosteradorkor@icloud.com>
2020-11-16 13:29:02 +0000

    first commit

---
+++ app2.js
@@ -0,0 +1,55 @@
+var express = require('express')
+
+var app = express()
+
+var fetch = require('node-fetch')
+
+
+
+//setting templating engine
+app.set('view engine', 'ejs');
+
+// setup static files
+app.use(express.static('public'));
+
+
+app.get('/', (request, response) => {
+    response.render('acity.ejs',
+        // Javascript object : JSON
+        {
+            "dog": "Foster",
+            "fish": "Getrude"
+        }
+    );
+});
+
+
+app.get('/users', (request, response) => {
+
+    // JS promise
+    fetch('https://randomuser.me/api/?results=5000')
+
+        .then(val => val.json())
+        .then(val => {
+            response.render('users.ejs', {
+                data: val.results
+            })
+        })
+
+})
+
+app.listen(5000)
+
+
+
+
+
+
+// var d = [
+//     {
+//         name: "foster",
+//         age: 12.5,
+//         hobbies: ["eating", "sleeping"]
+//     }
+// ]
+
